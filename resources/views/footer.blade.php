040144c3d89e11df0f9e9e42ea8abf6643381474
QUANG LIC <quang.lic55@gmail.com>
2025-03-17 18:50:36 +0700

    Update footer.blade.php

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -1,4 +1,3 @@
-
 </div>
 <!-- /.card-footer -->
 <!-- /.card-footer -->
@@ -8,45 +7,10 @@
 <!-- /.col -->
 </div>
 
-<div class="modal fade" id="moveModal" tabindex="-1" role="dialog" aria-labelledby="moveModalLabel">
-    <div class="modal-dialog" role="document">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h4 class="modal-title" id="moveModalLabel">Rename & Move</h4>
-                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-            </div>
-            <form id="file-move">
-                <div class="modal-body">
-                    <div class="form-group">
-                        <label for="recipient-name" class="control-label">Path:</label>
-                        <input type="text" class="form-control" id="moveModel_new" name="new" />
-                    </div>
-                    <input type="hidden" id="moveModel_path" name="path"/>
-                </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-light btn-sm" data-bs-dismiss="modal">Close</button>
-                    <button type="submit" class="btn btn-primary btn-sm">Submit</button>
-                </div>
-            </form>
-        </div>
-    </div>
-</div>
-
 <div class="modal fade" id="urlModal" tabindex="-1" role="dialog" aria-labelledby="urlModalLabel">
     <div class="modal-dialog" role="document">
         <div class="modal-content">
-            <div class="modal-header">
-                <h4 class="modal-title" id="urlModalLabel">Url</h4>
-                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-            </div>
-            <div class="modal-body">
-                <div class="form-group">
-                    <input type="text" class="form-control" />
-                </div>
-            </div>
-            <div class="modal-footer">
-                <button type="button" class="btn btn-light btn-sm" data-bs-dismiss="modal">Close</button>
-            </div>
+            <iframe id="urlModal_Frame" src="https://lic-ems.local/ems/messages/share?file=" title="Chia sẻ tới nhóm chat"></iframe>
         </div>
     </div>
 </div>
@@ -63,7 +27,7 @@
                     <div class="form-group">
                         <input type="text" class="form-control" name="name" />
                     </div>
-                    <input type="hidden" name="dir" value="{{ $url['path'] }}"/>
+                    <input type="hidden" name="dir" value="{{ $url['path'] }}" />
                     {{ csrf_field() }}
                 </div>
                 <div class="modal-footer">
@@ -74,3 +38,21 @@
         </div>
     </div>
 </div>
+
+<style>
+    .modal-dialog {
+        position: fixed;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        width: 100vw;
+        height: 100vh;
+        max-width: none !important;
+    }
+
+    .model-content {
+        width: 500px;
+        height: 400px;
+        position: relative;
+    }
+</style>
